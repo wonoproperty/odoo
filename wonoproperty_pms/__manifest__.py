d54b6064a94704e97f11a08332d19724009c2502
Maurice Jansz <mauricejansz@gmail.com>
2022-02-24 08:45:49 +0530

    [IMP] Adding new fields to tenant history

--- wonoproperty_pms/__manifest__.py
+++ wonoproperty_pms/__manifest__.py
@@ -5,7 +5,7 @@
     'description': 'Add custom Property Management System for WonoProperty',
     'author': 'Maurice Jansz',
     'category': 'Property Management',
-    'version': '1.0.2',
+    'version': '1.0.3',
     'depends': ['account'],
     'data': [
         'data/expense_type_data.xml',
