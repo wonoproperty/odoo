e259921f9b7dd1ae5748f34237d2a5af9c8a1efc
Maurice Jansz <mauricejansz@gmail.com>
2022-03-06 15:00:35 +0530

    [IMP] Adding invoice template

--- wonoproperty_pms/models/account_move.py
+++ wonoproperty_pms/models/account_move.py
@@ -5,8 +5,14 @@ from odoo import fields, models
 class AccountMoveProperty(models.Model):
     _inherit = 'account.move'
 
+    def _amount_in_words(self):
+        for rec in self:
+            rec.amount_in_words = str(rec.currency_id.amount_to_text(rec.amount_total))
+
     property_unit_id = fields.Many2one('property.unit', string='Property')
     property_expense_id = fields.Many2one('expense.type', string='Expense')
     date_from = fields.Date(string='Date From')
     date_to = fields.Date(string='Date To')
     tenant_history_id = fields.Many2one('tenant.history', string='Tenant History')
+    amount_in_words = fields.Char(string="Amount In Words", compute='_amount_in_words')
+
