e7f336b84134334e23c6b30d4e7788105c33be28
Maurice Jansz <mauricejansz@gmail.com>
2022-07-11 09:50:14 +0530

    [IMP] Adding archive option

--- wonoproperty_pms/__manifest__.py
+++ wonoproperty_pms/__manifest__.py
@@ -5,7 +5,7 @@
     'description': 'Add custom Property Management System for WonoProperty',
     'author': 'Maurice Jansz',
     'category': 'Property Management',
-    'version': '1.0.14',
+    'version': '1.0.15',
     'depends': ['web', 'account'],
     'data': [
         'data/expense_type_data.xml',
